43347d65c85bb399fa4c49a144609d2af340bd78
Prince Solomon 💻 <77865154+Princexz@users.noreply.github.com>
2024-09-23 18:10:52 +0100

    Update typed.init.js

--- js/typed.init.js
+++ js/typed.init.js
@@ -1,6 +1,6 @@
 //keyword typing
 $(".element").typed({
-    strings: ["Prince Solomon", "a Full-Stack Developer", "a Software Engineer", "a Computer Programmer"],
+    strings: ["Prince Solomon", "a Full-Stack Developer", "a Software Engineer", "a IT Support Engineer", "a Computer & Robotic Engineer"],
     typeSpeed: 10,
     loop:true,
     backDelay: 1800
